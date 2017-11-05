6c6a2502d300bae17e77dc682f3bc47fd7e9a58d
mszewczyk <szewczykm94@gmail.com>
2017-11-05 19:02:14 +0100

    Decorators examples

--- notes/decorators.py
+++ notes/decorators.py
@@ -1,7 +1,50 @@
+# passing functions as arguments to another functions:
+import timeit
+
+
+def func(fn, *args, **kwargs):
+    fn(*args, **kwargs)
+
+
+# and invocation:
+func(lambda x: print(x), 10)
+
+
+# it is also possible to return function from the function:
+def func_outer(x):
+    def func_inner(y):
+        print(str(x) + str(y))
+
+    return func_inner
+
+
+# and invocation:
+func_outer('first and ')('second')
+
+
+# Decorators - use both approaches
+def decorator(fn):
+    def decorator_method(*args, **kwargs):
+        print('Invocation of method: ' + str(fn))
+        result = fn(*args, **kwargs)
+        print('Method executed.')
+        return result
+
+    return decorator_method
+
+
+@decorator
+def method(arg):
+    print('executing method with arg: {}!'.format(arg))
+
+
+method('foo')
+
+
 def decorator(func):
-    def decorating():
+    def decorating(*args, **kwargs):
         print('decorating!')
-        return func()
+        return func(*args, **kwargs)
 
     return decorating
 
@@ -10,4 +53,26 @@ def decorator(func):
 def decorated():
     print('Hello!')
 
+
 decorated()
+
+
+# decorators are extremely useful when it comes to perform some instrumentation:
+def time_measure_decorator(fn):
+    def decorate(*args, **kwargs):
+        timer = timeit.Timer(lambda: fn(*args, **kwargs))
+        execution_time = timer.timeit()
+        print(execution_time)
+
+    return decorate
+
+
+@time_measure_decorator
+def slow_method(n):
+    for i in range(n):
+        for j in range(i):
+            ignore = j ** i
+
+
+slow_method(2)
+
