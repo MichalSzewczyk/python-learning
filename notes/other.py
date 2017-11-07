76ae42c520e5e94479c53f959ddb30f854f92107
mszewczyk <szewczykm94@gmail.com>
2017-11-07 21:30:57 +0100

    Example of dynamic method addition

--- notes/other.py
+++ notes/other.py
@@ -1,4 +1,23 @@
 def get_type_of(*args):
     print(type(args))
 
+
 get_type_of()
+
+
+# It is possible to dynamically add methods to objects of class:
+class Foo:
+    def __init__(self):
+        pass
+
+
+foo_object = Foo()
+
+
+def bar_method():
+    print('Executing bar method.')
+
+
+foo_object.method = bar_method
+
+foo_object.method()
