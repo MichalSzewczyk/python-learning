8482fae35769faaac3e481b8965fb84ef81a89fd
mszewczyk <szewczykm94@gmail.com>
2017-11-07 22:23:41 +0100

    Added stateful decorator

--- notes/other.py
+++ notes/other.py
@@ -21,3 +21,41 @@ def bar_method():
 foo_object.method = bar_method
 
 foo_object.method()
+
+
+# As a consequence of fact that functions are objects in python, we can dynamically add function to function object:
+
+def foo():
+    print('foo function')
+
+
+def bar():
+    print('bar function')
+
+
+foo.bar = bar
+
+foo()
+foo.bar()
+
+
+# So indicates fact that we can create stateful decorators with functions to modify their state:
+
+class decorator:
+    def __init__(self, fn):
+        self.fn = fn
+        self.fn.cache_reset = self.cache_reset
+
+    def __call__(self, *args, **kwargs):
+        self.fn(*args, **kwargs)
+
+    def cache_reset(self):
+        print('success!')
+
+
+@decorator
+def test():
+    print('test')
+
+test()
+test.cache_reset()
