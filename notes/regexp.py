69983744c412c46c96b6b5074458befd399924c8
mszewczyk <szewczykm94@gmail.com>
2018-01-09 21:38:16 +0100

    basic regexp

---
+++ notes/regexp.py
@@ -0,0 +1,22 @@
+# Basics:
+# \d        - any digit
+# \s        - any whitespace
+# \w        - any alphanumeric letters
+# \D        - any non digit
+# \S        - any non whitespace
+# \w        - any non alphanumeric
+# \b        - boundary between word and non word
+# \w+\b     - word
+# [^a]      - any character but a
+# \s        - any whitespace
+# \t        - tab
+# \n        - newline
+# ^         - starts with
+# $         - ends with
+# ()        - define group
+# a{1,n}    - from 1 to n occurrences of a
+# (cats|dogs)   - or
+# ^(.*).pdf$
+# ((\w{3})\s(\d{4}))    - subgroups
+# ((\d{3,4})x((\d{3,4})))
+
