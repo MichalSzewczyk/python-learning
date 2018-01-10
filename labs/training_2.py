cb0ff06a6b94ef5aa23adcfd2672365ceecaab56
mszewczyk <szewczykm94@gmail.com>
2018-01-10 07:11:46 +0100

    t2

--- labs/training_2.py
+++ labs/training_2.py
@@ -1,6 +1,8 @@
 # Przygotuj funkcję, która usunie duplikaty z listy.
 import collections
 
+from collections import defaultdict
+
 my_list = ['art', 'lean', 'desk', 'flavor', 'compare', 'secretive', 'narrow', 'flavor', 'flavor', 'miami', 'teaching',
            'fire', 'rate', 'light', 'jump', 'offer', 'fold', 'abstract', 'box', 'story', 'bomb', 'grape', 'grin',
            'jackhammer', 'torchlight', 'flugelhorn', 'verkrampte', 'grandchild', 'witchcraft', 'pawnbroker',
@@ -40,5 +42,58 @@ def f1(message1, message2):
 
 
 print(f1("abarypxtw", "orakkkasdto"))
+
+import itertools
+
+
+def bin_num_gen(N):
+    """ Funkcja generuje liste stringów, reprezentujących kolejne liczby binarne od 0 do 2^n.
+    Bonusowe punkty za one-liner"""
+    return ["{0:b}".format(i) for i in range(0, 2 ** N)]
+
+
+print(bin_num_gen(4))
+print(bin_num_gen(4) == ['0000', '0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000', '1001', '1010',
+                         '1011', '1100', '1101', '1110', '1111'])
+
+
+def make_big_and_ordered(words, order):
+    """ Funkcja przyjmuje listę słów oraz listę liczb od 1 do N, gdzie N = długość listy słów.
+        Zwraca string złożony ze słów rzutowanych na duże listery, w kolejności zgodnej z order
+        Bonus za one-liner"""
+    return ' '.join(
+        i.upper() for i in map(lambda value: value[0], sorted(zip(words, order), key=lambda value: value[1])))
+
+
+words = ['kota', 'ala', 'ma']
+order = [3, 1, 2]
+
+print(make_big_and_ordered(words, order))
+print(make_big_and_ordered(words, order) == "ALA MA KOTA")
+
+# Dlaczego powyższy kod nie działa? Poprawić go.
+import copy
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [a, b]
+
+d = copy.deepcopy(c)
+c[0][0] = "A"
+assert (d[0][0] == 1)
+
+people = [('Smith', ('C++', 'Advanced')),
+          ('Mary', ('Java', 'Beginner')),
+          ('John', ('Python', 'Intermediate')),
+          ('Smith', ('Java', 'Intermediate')),
+          ('Agnes', ('Python', 'Beginner')),
+          ('Mary', ('C#', 'Advanced'))]
+
+d = defaultdict(lambda: [])
+
+for person, skill in people:
+    d[person].append(skill)
+
+print(list(d.items()))
 
 
